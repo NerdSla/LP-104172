87eecd41e0cf965d7131208062d28bab2a3f69e2
NerdSla <joaolbc1805@gmail.com>
2026-09-09 13:59:20 -0300

    Refactor retirement check to use birth year

--- PYTHON/empregado_aposentadoria.py
+++ PYTHON/empregado_aposentadoria.py
@@ -2,9 +2,9 @@ import os
 os.system('cls')
 
 codigo = input('Digite o código do empregado: ')
-idade = int(input('Digite sua idade: '))
+ano_de_nascimento = int(input('Digite seu ano de nascimento: '))
 tempo_trabalho = int(input('Digite o tempo de trabalho (em anos): '))
-
+idade = 2026 - ano_de_nascimento
 print(f'Código do empregado: {codigo}')
 print(f'Idade: {idade}')
 print(f'Tempo de trabalho: {tempo_trabalho} anos')
